c8bc127fffc51d3358c5ac4b5e68b93b3e775ace
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2017-01-21 18:32:41 -0500

    - Added the deployment files

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -10,7 +10,7 @@ export default class extends Phaser.State {
     centerGameObjects([this.loaderBg, this.loaderBar]);
 
     this.load.setPreloadSprite(this.loaderBar);
-    
+
     //
     // load your assets
     //
@@ -25,7 +25,7 @@ export default class extends Phaser.State {
     this.load.image('ufo', 'assets/images/ufo.png');
     this.load.image('waveman', 'assets/images/waveman.png');
     this.load.image('background', 'assets/images/starfield.png');
-    this.load.shader('stars', 'assets/shaders/stars.frag')
+    this.load.shader('stars', 'assets/shaders/stars.frag');
   }
 
   create() {
