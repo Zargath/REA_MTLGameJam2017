e6d20f5cd0c7be351cbdf580a69f3f7e3586fb15
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2017-01-21 01:04:00 -0500

    - Changed the game width

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import GameState from './states/Game';
 class Game extends Phaser.Game {
 
   constructor() {
-    const width = document.documentElement.clientWidth > 768 ? 768 : document.documentElement.clientWidth;
+    const width = document.documentElement.clientWidth < 768 ? 768 : document.documentElement.clientWidth;
     const height = document.documentElement.clientHeight > 1024 ? 1024 : document.documentElement.clientHeight;
 
     super(width, height, Phaser.AUTO, 'content', null);
