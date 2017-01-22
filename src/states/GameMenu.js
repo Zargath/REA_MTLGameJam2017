44815cb356ee20bc8b6e4ba0e6289384bcd65a42
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2017-01-22 10:35:24 -0500

    - Fixed text placement

--- src/states/GameMenu.js
+++ src/states/GameMenu.js
@@ -19,20 +19,25 @@ export default class extends Phaser.State {
 
     const colorGenerator = new ColorGenerator();
 
-    const xPos = (document.documentElement.clientWidth - 800) / 1.2;
-    const yPos = 20;
+    const xPos = document.documentElement.clientWidth / 2;
+    const yPos = (document.documentElement.clientHeight - 600) + 40;
 
     const randomHue = Math.random() * 360;
     const fillColor = colorGenerator.getRBGString(colorGenerator.HSVToRGB(randomHue, 0.5, 0.99));
     const strokeColor = colorGenerator.getRBGString(colorGenerator.HSVToRGB(randomHue, 0.5, 0.40));
 
     const style = { font: 'bold 30pt Arial', fill: fillColor, align: 'left', wordWrap: true, wordWrapWidth: document.documentElement.clientWidth - (document.documentElement.clientWidth / 3) };
-    this.game.introText = this.game.add.text(xPos, yPos, this.game.title, style);
-    this.game.introText.stroke = strokeColor;
-    this.game.introText.strokeThickness = 6;
+    this.introText = this.game.add.text(xPos, yPos, this.game.title, style);
+    this.introText.stroke = strokeColor;
+    this.introText.strokeThickness = 6;
 
-    this.startButton = this.game.add.button(xPos + 100, yPos + 200, 'button_start', this.goToMenu, this);
-    this.startButton = this.game.add.button(xPos + 100, yPos + 350, 'button_howtoplay', this.goToMenu, this);
+    this.introText.anchor.set(0.5);
+
+    this.startButton = this.game.add.button(xPos, yPos + 200, 'button_start', this.goToMenu, this);
+    this.menuButton = this.game.add.button(xPos, yPos + 350, 'button_howtoplay', this.goToMenu, this);
+
+    this.startButton.anchor.set(0.5);
+    this.menuButton.anchor.set(0.5);
   }
 
   showHowToPlay() {
